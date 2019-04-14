dd89032e26f1e0bd1ca3fba29a89aa02b43b798f
daiki0730 <xcrqf313@yahoo.cp.jp>
2019-04-14 23:50:34 +0900

    グループ作成機能を実装
    グループ機能作成のため

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,24 @@
 class GroupsController < ApplicationController
 
-def new
-end
+  def index
+  end
 
-def create
-end
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
 
-def edit
-end
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
+  end
 
-def update
-end
-
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
 end
